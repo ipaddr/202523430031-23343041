a1e1232917ca04250ccdf400db8cacf11f52311d
Isrezal Akbar <isrezalakbar03@gmail.com>
2026-03-03 23:40:33 +0700

    Deleting Existing Notes in Notes View

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutterdemo/constants/routes.dart';
 import 'package:flutterdemo/services/auth/auth_exceptions.dart';
 import 'package:flutterdemo/services/auth/auth_service.dart';
-import 'package:flutterdemo/utilities/show_error_dialog.dart';
+import 'package:flutterdemo/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
